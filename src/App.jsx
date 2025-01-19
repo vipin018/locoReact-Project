a4fd43ac60a3e39f9456b129386b6ca7a13c6626
vipin018 <vipinpathak018@gmail.com>
2025-01-19 14:43:41 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Page1 from './components/Page1'
+import Page2 from './components/Page2'
+import Page3 from './components/Page3'
 
-function App() {
-  const [count, setCount] = useState(0)
+import LocomotiveScroll from 'locomotive-scroll'
 
+const scroll = new LocomotiveScroll()
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Page1 />
+        <Page2 />
+        <Page3 />
+    </div>
   )
 }
 
