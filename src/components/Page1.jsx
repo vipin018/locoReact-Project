c03f2ec9448fb606ebc2f036a87ae8d4d903ca88
vipin018 <vipinpathak018@gmail.com>
2025-01-19 18:39:42 +0530

    updated

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Page1 = () => {
     return (
         <div className='page1'>
-            <h1>Page1</h1>
+           <h1>
+            <em>We are a </em>CREATIVE <em>studio</em> DEDICATED <em>to</em> CULTURAL <br />
+           ADVANCEMENT <em>through</em> STRATEGY <em>and</em> DESIGN.</h1>
         </div>
     )
 }
